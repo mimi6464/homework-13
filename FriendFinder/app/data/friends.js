526075de20007b7a0829908de90dec2fff5926b5
tsionasmamaw <tayimer@yahoo.com>
2016-09-28 12:20:06 -0700

    adding server

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,35 @@
+var friendData = [{
+	    		name: "jaguars animal",
+	    		photo: "https://www.google.com/imgres?imgurl=http://yourshot.nationalgeographic.com/u/ss/fQYSUbVfts-T7pS2VP2wnKyN8wxywmXtY0-FwsgxoJZhLhqrfToTV6cQgBT27ZjBszkurRIRMNLzd_FjeYfp/&imgrefurl=http://animals.nationalgeographic.com/animals/mammals/jaguar/&h=741&w=960&tbnid=7td2QBD7pBItfM:&tbnh=155&tbnw=200&docid=5WaCx-aZJWWlXM&itg=1&usg=__lsmiRNxoSM8uH9hgzp8nQQBm8d0=",
+	    		scores:  
+	    }, {	
+	    		name: "gitaffe",
+	    		photo: "https://www.google.com/imgres?imgurl=http://www.livescience.com/images/i/000/068/094/i300/giraffe.jpg%3F1405008442%3Finterpolation%3Dlanczos-none%26downsize%3D192:*&imgrefurl=http://www.livescience.com/27336-giraffes.html&h=288&w=192&tbnid=CiieimilXkGjRM:&tbnh=186&tbnw=124&docid=mwHgCiDw14CNeM&itg=1&usg=__TBq2dK_UtcA_XuhrYRYKHiXtdTc=",
+	    		scores:  
+	    }, {
+	    		name: "gorilla",
+	    		photo: "https://www.google.com/imgres?imgurl=https://upload.wikimedia.org/wikipedia/commons/5/50/Male_gorilla_in_SF_zoo.jpg&imgrefurl=https://en.wikipedia.org/wiki/Gorilla&h=2488&w=2228&tbnid=Ppdu2z35vlTS4M:&tbnh=186&tbnw=166&docid=PPD4hUL67Vhq1M&itg=1&usg=__JWEiBgRGEAZ5YDcr4rkRBkwU_tk=",
+	    		scores:  
+	    }, {				
+	    		name: "lion",
+	    		photo: "https://www.google.com/imgres?imgurl=http://cdn.images.express.co.uk/img/dynamic/galleries/x701/67639.jpg&imgrefurl=http://www.express.co.uk/pictures/galleries/3038/Things-you-didn-t-know-about-lions-World-Lion-Day-facts-animals-pictures&h=701&w=898&tbnid=dMAiS3vuzfjhnM:&tbnh=156&tbnw=199&docid=5G2bAOi9tlIggM&itg=1&usg=__tCNHUFnWsGLyibw18fOerbLpJvI=",
+	    		scores:  
+	    }, {		
+
+	    		name: "dolphin",
+	    		photo: "https://www.google.com/search?q=lion+dolphin&espv=2&biw=1223&bih=754&tbm=isch&imgil=h8qzFEfwMhBAPM%253A%253B4Z4OwWGo-Lt3TM%253Bhttp%25253A%25252F%25252Fwww.triptopuntacana.com%25252Fproduct%25252Fextreme-dolphin-adventure%25252F&source=iu&pf=m&fir=h8qzFEfwMhBAPM%253A%252C4Z4OwWGo-Lt3TM%252C_&usg=__zwpxmfu0Ies3vpMGWayX729YwFo%3D&ved=0ahUKEwiY_aW6rLLPAhUX7WMKHY0VAGoQyjcIKQ&ei=rd3rV9jyIZfajwONq4DQBg#imgdii=h8qzFEfwMhBAPM%3A%3Bh8qzFEfwMhBAPM%3A%3BCU3k07jVv0nrAM%3A&imgrc=h8qzFEfwMhBAPM%3A",
+	    		scores:  
+	    }, {		
+
+
+
+
+
+
+
+
+
+
+
+
+    }];
---
+++ FriendFinder/app/routing/api-routes.js
@@ -0,0 +1,87 @@
+//GET route with the url/api/friends. will be used to display a JSON OF ALL possible friends
+
+// ===============================================================================
+ // LOAD DATA
+ // We are linking our routes to a series of "data" sources.
+ // These data sources hold arrays of information on table-data, waitinglist, etc.
+ // ===============================================================================this is just example
+ 
+ // var tableData 		= require('../data/table-data.js');
+ var friendData 	= require('../data/waitinglist-data.js');
+ 
+ 
+ // ===============================================================================
+ // ROUTING
+ // ===============================================================================
+ 
+ module.exports = function (app) {
+ 	// API GET Requests
+ 	// Below code handles when users "visit" a page.
+ 	// In each of the below cases when a user visits a link
+ 	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+ 	// ---------------------------------------------------------------------------
+ 
+ 	// app.get('/api/tables', function (req, res) {
+ 	// 	res.json(tableData);
+ 	// });
+ 
+ 	app.get('/api/friends', function (req, res) {
+ 		res.json(friendData);
+ 	});
+ 
+ 	// API POST Requests
+ 	// Below code handles when a user submits a form and thus submits data to the server.
+ 	// In each of the below cases, when a user submits form data (a JSON object)
+ 	// ...the JSON is pushed to the appropriate Javascript array
+ 	// (ex. User fills out a reservation request... this data is then sent to the server...
+ 	// Then the server saves the data to the tableData array)
+ 	// ---------------------------------------------------------------------------
+ 
+ 	app.post('/api/tables', function (req, res) {
+ 	// 	// Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+ 	// 	// It will do this by sending out the value "true" have a table
+ 	// 	if (tableData.length < 5) {
+ 	// 		tableData.push(req.body);
+ 	// 		res.json(true); // KEY LINE
+ 	// 	} else { // Or false if they don't have a table
+ 	// 		waitListData.push(req.body);
+ 	// 		res.json(false); // KEY LINE
+ 	// 	}
+ 	// });
+ user x = [5, 1, 4, 4, 5, 1, 2, 5, 4, 1];
+ user y = [3, 2, 6, 4, 5, 1, 2, 5, 4, 1];
+
+
+ function compareTwoArrays(arr1, arr2){
+  var result = 0;
+  
+  for(var i=0; i<arr1.length; i++){
+    if(arr1[i] !== arr2[i]){
+      if(arr1[i] > arr2[i]){
+        result += (arr1[i] - arr2[i]);
+      } else{
+        result += (arr2[i] - arr1[i]);
+      }
+    }
+  }
+  
+  return result;
+}
+
+
+var ans = compareTwoArrays(x, y); 
+
+console.log(ans);
+ 	// ---------------------------------------------------------------------------
+ 	// I added this below code so you could clear out the table while working with the functionality.
+ 	// Don't worry about it!
+ 
+ 	app.post('/api/clear', function () {
+ 		// Empty out the arrays of data
+ 		tableData = [];
+ 		waitListData = [];
+ 
+ 		console.log(tableData);
+ 	});
+ };
+...	...
