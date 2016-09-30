cb09fb1092bb9642024dc181abfb51919a3ef07c
tsionasmamaw <tayimer@yahoo.com>
2016-09-29 17:32:46 -0700

    modified again

--- FriendFinder/app/routing/api-routes.js
+++ FriendFinder/app/routing/api-routes.js
@@ -65,34 +65,21 @@ function findTheOne(user){
 }
   res.json(findTheOne(friendsData[friendsData.length-1]));
     
-  });
-
-
-  // API GET Requests
-  app.get('/api/friends', function(req, res){
-    res.json(friendsData);  
-  });
+  
   
 
-  // API POST Requests
-  app.post('/api/friends', function(req, res){
-      friendsData.push(req.body);
-      res.json(true); // KEY LINE
-  });
-
-
   // resets everything
-  app.post('/api/clear', function(req, res){
+  // app.post('/api/clear', function(req, res){
     // Empty out the arrays of data
-    friendsData = [
-      {
-        "name" :  "tsion",
-        "photo": "https://s-media-cache-ak0.pinimg.com/236x/8d/a6/cf/8da6cfe5c7116c1a03de6ddfcdb2d267.jpg",
-        "scores": [3, 3, 5, 4, 4, 5, 2, 4, 5, 5]
-      }
-    ];
+  //   friendsData = [
+  //     {
+  //       "name" :  "tsion",
+  //       "photo": "https://s-media-cache-ak0.pinimg.com/236x/8d/a6/cf/8da6cfe5c7116c1a03de6ddfcdb2d267.jpg",
+  //       "scores": [3, 3, 5, 4, 4, 5, 2, 4, 5, 5]
+  //     }
+  //   ];
 
-  })
+  // })
 }
 
 
