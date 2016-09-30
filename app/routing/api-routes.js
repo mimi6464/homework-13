ead01b8c76f7e7332768ff1ca48bfaae47153e98
tsionasmamaw <tayimer@yahoo.com>
2016-09-30 12:08:39 -0700

    some

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -5,79 +5,74 @@
 
 // ========================================================================
 var friendsData = require('../data/friends.js')
-var storeArray = require('../data/data.js');
-
+    // var storeArray = require('../data/data.js');
+var path = require('path');
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app) {
-        // API GET Requests
+    // API GET Requests
 
-        app.get('/api/friends', function(req, res) {
-            res.json(friendsData);
-        });
+    // app.get('/api/friends', function(req, res) {
+    //     res.json(friendsData);
+    // });
 
+    // API POST Requests
 
+    app.post('/api/friends', function(req, res) {
 
-        // API POST Requests
-
-        app.post('/api/friends', function(req, res) {
-                //  // Note the code here. Our "server" will respond to requests and let users know if they have a table or not
-                storeArray.push(req.body.scores);
+        friendsData.push(req.body.scores);
 
 
-                function compareTwoArrays(arr1, arr2) {
-                    var result = 0;
+        function compareTwoArrays(arr1, arr2) {
+            var result = 0;
 
-                    for (var i = 0; i < arr1.length; i++) {
-                        if (arr1[i] !== arr2[i]) {
-                            if (arr1[i] > arr2[i]) {
-                                result += (arr1[i] - arr2[i]);
-                            } else {
-                                result += (arr2[i] - arr1[i]);
-                            }
-                        }
+            for (var i = 0; i < arr1.length; i++) {
+                if (arr1[i] !== arr2[i]) {
+                    if (arr1[i] > arr2[i]) {
+                        result += (arr1[i] - arr2[i]);
+                    } else {
+                        result += (arr2[i] - arr1[i]);
                     }
-                    return result;
                 }
+            }
+            return result;
+        }
 
 
 
-                function findTheOne(user) {
-                    var scoresUnsorted = [];
-                    var scoresSort = [];
-                    for (var i = 0; i < friendsData.length; i++) {
-                        scoresSort.push(compareTwoArrays(user.scores, friendsData[i].scores));
-                    }
+        function findTheOne(user) {
+            var scoresUnsorted = [];
+            var scoresSort = [];
+            for (var i = 0; i < friendsData.length; i++) {
+                scoresSort.push(compareTwoArrays(user.scores, friendsData[i].scores));
+            }
 
-                    scoresSort.forEach(function(item) {
-                        scoresUnsorted.push(item);
-                    });
-
-                    var sorted = scoresSort.sort(function(a, b) {
-                        return a - b;
-                    });
-
-                    var theIndex = scoresUnsorted.indexOf(sorted[0]);
-
-                    return friendsData[theIndex].name;
-                }
-
-
-                res.json(findTheOne(friendsData[friendsData.length - 1]));
-
+            scoresSort.forEach(function(item) {
+                scoresUnsorted.push(item);
             });
 
-
-
-
-
-            app.post('/api/clear', function() {
-                // Empty out the arrays of data
-                friendsData = [];
-
-
-                console.log(friendsData);
+            var sorted = scoresSort.sort(function(a, b) {
+                return a - b;
             });
-        };
+
+            var theIndex = scoresUnsorted.indexOf(sorted[0]);
+
+            return friendsData[theIndex].name;
+        }
+
+
+        res.json(findTheOne(friendsData[friendsData.length - 1]));
+
+    });
+
+
+    app.post('/api/clear', function() {
+        // Empty out the arrays of data
+        friendsData = [];
+
+
+        console.log(friendsData);
+    });
+};
